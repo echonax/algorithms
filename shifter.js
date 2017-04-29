9cb4ff35a4916e6606659c65b7ee988df3cb4a9f
Ekin <ccam@ku.edu.tr>
2017-04-29 23:02:12 +0300

    Update shifter.js

--- shifter.js
+++ shifter.js
@@ -2,7 +2,7 @@
 //Takes an array and index, starts from index and including the index shifts the next 2 elements to left by 1
 //Ex: 1432 -> arr,1 -> 1324
 function shiftToLeft(arr, ind){
-	if(arr[ind+1] && arr[ind+2]){
+  if(arr[ind+1] && arr[ind+2]){
     var temp1 = arr[ind+1];
     var temp2 = arr[ind+2];
     arr[ind+2] = arr[ind];
